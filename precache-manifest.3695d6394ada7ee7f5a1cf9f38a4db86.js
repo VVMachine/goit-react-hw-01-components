9747df5cc221cb78a27e329e894a47e569804037
VVMachine <vik.atamanenko@gmail.com>
2020-09-23 10:19:03 +0300

    Updates

---
+++ precache-manifest.3695d6394ada7ee7f5a1cf9f38a4db86.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df9a3f67cd88a6869a88daa712e5fc12",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "6646b07f757272a00ba1",
+    "url": "/goit-react-hw-01-components/static/css/main.6a66d8a9.chunk.css"
+  },
+  {
+    "revision": "a2ea0e24f79ada96a293",
+    "url": "/goit-react-hw-01-components/static/js/2.6983a254.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-01-components/static/js/2.6983a254.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6646b07f757272a00ba1",
+    "url": "/goit-react-hw-01-components/static/js/main.f1f01306.chunk.js"
+  },
+  {
+    "revision": "7c1abe1df921e466788f",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.e943b22f.js"
+  }
+]);
